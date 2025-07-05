c32120252868ad4f182be0c85faac6394fabd79c
Mr.Bom <bombeamlazer@gmail.com>
2025-07-05 18:49:22 +0700

    menu

--- Assets/Bomb/Script/MainMenu.cs
+++ Assets/Bomb/Script/MainMenu.cs
@@ -1,16 +1,22 @@
 using UnityEngine;
+using UnityEngine.SceneManagement;
+using System.Collections;
+using System.Collections.Generic;
 
-public class MainMenu : MonoBehaviour
-{
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+
+public class MainMenu : MonoBehaviour {
+
+    public string nextSceneName = "Main"; //‡Õ“‰ø≈Ï®“°‡¡π
+
+    public void PlayGame ()
     {
         
+        SceneManager.LoadScene(nextSceneName);
     }
 
-    // Update is called once per frame
-    void Update()
+    public void QuitGame ()
     {
-        
+        Debug.Log("Quit!");
+        Application.Quit();
     }
 }
